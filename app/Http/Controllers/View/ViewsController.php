0789011ce940895f1983d974528a7d1527f1dea6
guicheng <996719827@qq.com>
2021-10-11 22:51:40 +0800

    Line auth

--- app/Http/Controllers/View/ViewsController.php
+++ app/Http/Controllers/View/ViewsController.php
@@ -19,8 +19,8 @@ class ViewsController extends Controller
     public function login(Request $request)
     {
         $lineOauth = 'https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=' .
-            env('LINE_CLIENT_ID') . '&redirect_uri=' . urlencode(config('app.url')) . '/line/callback' . '&state=12345abcde' .
-            '&scope=profile%20openid&nonce=09876xyz';
+            env('LINE_CLIENT_ID') . '&redirect_uri=' . urlencode(config('app.url')) . '/line/callback' .
+            '&state=' . time() . '&scope=profile%20openid&nonce=09876xyz';
         return view('login', ['lineOauth' => $lineOauth]);
     }
 
