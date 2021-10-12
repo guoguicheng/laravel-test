ae3db50697823e868d60dd30211cc5110936d2e8
guicheng <996719827@qq.com>
2021-10-12 14:16:37 +0800

    line bot

---
+++ database/migrations/2021_10_12_061323_modify_line_openid_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyLineOpenidToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('line_openid')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+        });
+    }
+}
