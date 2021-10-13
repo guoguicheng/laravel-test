31dd667669af9ce3b7afcd0d4c6f40d3cc3999ad
guicheng <996719827@qq.com>
2021-10-13 10:36:38 +0800

    fix:刷新token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,21 +10,20 @@ class AuthController extends Controller
 {
     public function refreshToken(Request $request, Client $http)
     {
-        $accessToken = $request->header('Authorization');
-        if (empty($accessToken)) {
-            throw new ApiException('Authorization头获取失败');
+        $params = $request->only(['refresh_token']);
+        if (empty($params['refresh_token'])) {
+            throw new ApiException('refresh_token获取失败');
         }
         $resp = $http->post(config('app.url') . '/oauth/token', [
             'form_params' => [
                 'grant_type' => 'refresh_token',
                 'client_id' => env('CLIENT_ID', ''),
                 'client_secret' => env('CLIENT_SECRET', ''),
-                'refresh_token' => $accessToken
+                'refresh_token' => $params['refresh_token']
             ]
         ]);
-
         $token = json_decode((string)$resp->getBody(), true);
 
-        return response()->json($token);
+        return response()->json(['token' => $token]);
     }
 }
